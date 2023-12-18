2b46c8973632804f2baa26179b50ec48c7264484
ZhangMuYu <zmy7135@qq.com>
2023-12-18 16:43:56 +0800

    组件新增

---
+++ AutoArchive/AutoArchivePlus/Component/NavigationMenuItem.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AutoArchivePlus.Component
+{
+    /// <summary>
+    /// NavigationMenuItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class NavigationMenuItem : UserControl
+    {
+        #region Properties
+
+        public static readonly DependencyProperty ItemNameProperty;
+
+        public static readonly DependencyProperty ItemIconProperty;
+
+        public static readonly DependencyProperty TestProperty;
+
+        #endregion
+
+        static NavigationMenuItem()
+        {
+            ItemNameProperty = DependencyProperty.Register("ItemName", typeof(string), typeof(FormTitleBar));
+            ItemIconProperty = DependencyProperty.Register("ItemIcon", typeof(ImageSource), typeof(FormTitleBar));
+            TestProperty= DependencyProperty.Register("Test", typeof(string), typeof(FormTitleBar));
+        }
+
+        public NavigationMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        public string Test
+        {
+            get
+            {
+                return (string)GetValue(TestProperty);
+            }
+            set
+            {
+                SetValue(TestProperty, value);
+            }
+        }
+
+        public string ItemName
+        {
+            get
+            {
+                return (string)GetValue(ItemNameProperty);
+            }
+            set
+            {
+                SetValue(ItemNameProperty, value);
+            }
+        }
+
+        public ImageSource ItemIcon
+        {
+            get
+            {
+                return (ImageSource)GetValue(ItemIconProperty);
+            }
+            set
+            {
+                SetValue(ItemIconProperty, value);
+            }
+        }
+    }
+}
---
+++ AutoArchive/AutoArchivePlus/Forms/Navigation.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AutoArchivePlus.Component;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoArchivePlus.Forms
+{
+    /// <summary>
+    /// Navigation.xaml 的交互逻辑
+    /// </summary>
+    public partial class Navigation : UserControl
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+        }
+
+        public void addMenu(String menuName, String icon = "pack://application:,,,/Resources/img/xinwang-ds3-ico.jpg")
+        {
+            NavigationMenuItem navigationMenuItem = new NavigationMenuItem();
+            navigationMenuItem.ItemName = menuName;
+            menu.Children.Add(navigationMenuItem);
+        }
+    }
+}
