ed64887b715b1a9e7bd913e3704bb059305469f4
ZhangMuYu <zmy7135@qq.com>
2024-03-07 15:23:44 +0800

    支持steamid

--- AutoArchive/SteamTool/Steam.cs
+++ AutoArchive/SteamTool/Steam.cs
@@ -11,35 +11,23 @@ namespace SteamTool
     public class Steam
     {
         public const String STEAM_REGISTRY_KEY_PATH = @"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam";
+        public const String STEAM_ACTIVE_PROCESS_PATH = @"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam\ActiveProcess";
         public const String STEAM_PATH_KEY = "SteamPath";
+        public const String STEAM_ACTIVE_USER_KEY = "ActiveUser";
         public const String STEAM_APPS_INSTALL_PATH = @"\common\";
         public const String STEAM_APPS = @"\steamapps\";
         public const String ACF_FILE = "*.acf";
         public const String LIBRARY_FOLDERS_VDF_FILE = "libraryfolders.vdf";
         public static List<String> EXCLUDE = new List<String>() { "Steamworks Common Redistributables" };
-
-        static String GetSteamPath()
-        {
-            return (String)Registry.GetValue(STEAM_REGISTRY_KEY_PATH, STEAM_PATH_KEY, "");
-        }
-
-        static List<LibraryFolder> GetAppInstalledPath()
-        {
-            String steamPath = Steam.GetSteamPath();
-            String appPath = steamPath + STEAM_APPS;
-            if (File.Exists(appPath + LIBRARY_FOLDERS_VDF_FILE))
-            {
-                return SteamFilePaser.parseVDF(appPath + LIBRARY_FOLDERS_VDF_FILE);
-            }
-            return null;
-        }
+        public static int steamUserId;
+        public static String steamPath;
 
         public static List<SteamAppInfo> GetInstalledApps()
         {
-            GetAppInstalledPath();
             List<AppConfiguration> appSaveInfos = readJson("GamesSaveDirectory.json", Encoding.UTF8);
             List<SteamAppInfo> steamAppInfos = new List<SteamAppInfo>();
-            String steamPath = Steam.GetSteamPath();
+            steamPath = GetSteamPath();
+            steamUserId = GetSteamActiveUserId();
             if (!String.IsNullOrEmpty(steamPath))
             {
                 List<LibraryFolder> Installfolders = GetAppInstalledPath();
@@ -80,34 +68,64 @@ namespace SteamTool
 
         public static void SaveGameInfos(String content)
         {
-            using (StreamWriter writer = new StreamWriter("GamesSaveDirectory.json",false))
+            using (StreamWriter writer = new StreamWriter("GamesSaveDirectory.json", false))
             {
                 writer.Write(content);
             }
+        }
+        private static String GetSteamPath()
+        {
+            return (String)Registry.GetValue(STEAM_REGISTRY_KEY_PATH, STEAM_PATH_KEY, "");
+        }
+
+        private static int GetSteamActiveUserId()
+        {
+            return (int)Registry.GetValue(STEAM_ACTIVE_PROCESS_PATH, STEAM_ACTIVE_USER_KEY, "");
+        }
+
+        private static List<LibraryFolder> GetAppInstalledPath()
+        {
+            String appPath = steamPath + STEAM_APPS;
+            if (File.Exists(appPath + LIBRARY_FOLDERS_VDF_FILE))
+            {
+                return SteamFilePaser.parseVDF(appPath + LIBRARY_FOLDERS_VDF_FILE);
+            }
+            return null;
         }
 
         private static void infoFix(AppConfiguration appSaveInfo, ref SteamAppInfo steamAppInfo)
         {
             if (appSaveInfo != null)
             {
-                String specialFolder = appSaveInfo.SavedPath;
                 String savedPath = appSaveInfo.SavedPath;
                 if (savedPath != null)
                 {
                     if (savedPath.StartsWith("%USERPROFILE%"))
                     {
-                        specialFolder = specialFolder.Replace("%USERPROFILE%", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+                        savedPath = savedPath.Replace("%USERPROFILE%", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
                     }
                     else if (savedPath.StartsWith("%CommonApplicationData%"))
                     {
-                        specialFolder = specialFolder.Replace("%CommonApplicationData%", Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
+                        savedPath = savedPath.Replace("%CommonApplicationData%", Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
                     }
                     else if (savedPath.StartsWith("%MyDocuments%"))
                     {
-                        specialFolder = specialFolder.Replace("%MyDocuments%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                        savedPath = savedPath.Replace("%MyDocuments%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                    }
+                    else if (savedPath.StartsWith("%STEAMINSTALL%"))
+                    {
+                        savedPath = savedPath.Replace("%STEAMINSTALL%",steamPath);
+                    }
+                    // replace userId
+                    if (steamUserId != 0)
+                    {
+                        if (savedPath.Contains("%Steam3AccountID%"))
+                        {
+                            savedPath = savedPath.Replace("%Steam3AccountID%", steamUserId.ToString());
+                        }
                     }
                 }
-                steamAppInfo.ArchivePath = specialFolder;
+                steamAppInfo.ArchivePath = savedPath;
                 steamAppInfo.ExecutablePath += appSaveInfo.Executable;
             }
         }
